9f403b1bbdeb58b7081af6306d70a219a9460f36
Kirill Merkushev <1964214+lanwen@users.noreply.github.com>
2026-06-19 20:46:25 +0200

    Cover JSpecify annotation placement fixtures

--- frmtr-core/src/test/resources/format/type-use-array-annotations/frmtr-default.output.java
+++ frmtr-core/src/test/resources/format/type-use-array-annotations/frmtr-default.output.java
@@ -1,20 +1,42 @@
+@NullMarked
 class TypeUseArrayAnnotations {
 
     private String @Nullable [] aliases;
 
     private @Nullable String @Nullable [] fallbackAliases;
 
+    private List<? extends @Nullable Number> limitedNumbers;
+
+    private List<? super @Nullable String> fallbackLabels;
+
+    private String[] @Nullable [] rows;
+
+    private @Nullable String @Nullable [] @Nullable [] matrix;
+
     void invoke(Object @Nullable [] args, @Nullable String @Nullable [] fallbackAliases) {
         String @Nullable [] localAliases;
         @Nullable
         String @Nullable [] localFallbackAliases;
+        String[] @Nullable [] localRows;
+        @Nullable
+        String @Nullable [] @Nullable [] localMatrix;
     }
 
     void relay(Object @Nullable ... args) {}
+
+    void relayLabels(@Nullable String... labels) {}
+
+    @NullUnmarked
+    void relayPlain(String label) {}
 }
 
 record TypeUseArrayRecord(
     String @Nullable [] aliases,
     @Nullable String @Nullable [] fallbackAliases,
-    Object @Nullable... args
+    List<? extends @Nullable Number> limitedNumbers,
+    List<? super @Nullable String> fallbackLabels,
+    String[] @Nullable [] rows,
+    @Nullable String @Nullable [] @Nullable [] matrix,
+    Object @Nullable... args,
+    @Nullable String... labels
 ) {}
